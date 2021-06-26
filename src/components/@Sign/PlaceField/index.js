a6ec1ef813a82db38af8afe8ed24bba47c43810f
Jones Napoleon Autumn <13518086@std.stei.itb.ac.id>
2021-06-26 21:53:17 +0700

    refakyou placefields

--- src/components/@Sign/PlaceField/index.js
+++ src/components/@Sign/PlaceField/index.js
@@ -8,16 +8,8 @@ import { useData } from "../../../contexts/DataContext";
 import { useAuth } from "../../../contexts/AuthContext";
 import "./placefield.css";
 
-// JOJO
-import {
-  Box,
-  Button,
-  SelectList,
-} from 'gestalt';
-import 'gestalt/dist/gestalt.css';
-// END JOJO
-
-const temp = "https://storage.googleapis.com/legaltech-esign-develop/develop/doc/aisc_jones_napoleon_pdf1624197842048";
+const temp =
+  "https://storage.googleapis.com/legaltech-esign-develop/develop/doc/aisc_jones_napoleon_pdf1624197842048";
 
 const PlaceField = ({
   activeItem,
@@ -37,25 +29,28 @@ const PlaceField = ({
   var assignees = getItemData(atr, "signers");
 
   // !!TODO DELETE kalo udah nambahin di /me
-  assignees.push({ email: "jonathanyudigun@gmail.com", name: "Jojo", flowtype: "SIGN" });
-  const assigneesValues = assignees.map(user => ({ value: user.email, label: user.name }));
-  let initialAssignee = assigneesValues.length > 0 ? assigneesValues[0].value : '';
+  assignees.push({
+    email: "jonathanyudigun@gmail.com",
+    name: "Jojo",
+    flowtype: "SIGN",
+  });
+  const assigneesValues = assignees.map((user) => ({
+    value: user.email,
+    label: user.name,
+  }));
+  let initialAssignee =
+    assigneesValues.length > 0 ? assigneesValues[0] : { value: "", label: "" };
   const [assignee, setAssignee] = useState(initialAssignee);
   // END JOJO
 
   const viewer = useRef(null);
-
-  console.log(fileData);
 
   useEffect(() => {
     if ((typeof fileData?.linkToPdf === "string" && auth?.id_token) || temp)
       WebViewer(
         {
           path: "webviewer",
-          disabledElements: [
-            "ribbons",
-            "toggleNotesButton"
-          ],
+          disabledElements: ["ribbons", "toggleNotesButton"],
         },
         viewer.current
       ).then(async (instance) => {
@@ -68,8 +63,8 @@ const PlaceField = ({
         // JOJO
         const { iframeWindow } = instance;
         const iframeDoc = iframeWindow.document.body;
-        iframeDoc.addEventListener('dragover', dragOver);
-        iframeDoc.addEventListener('drop', e => {
+        iframeDoc.addEventListener("dragover", dragOver);
+        iframeDoc.addEventListener("drop", (e) => {
           drop(e, instance);
         });
         // END JOJO
@@ -85,14 +80,14 @@ const PlaceField = ({
   // };
 
   // JOJO
-  const dragOver = e => {
+  const dragOver = (e) => {
     e.preventDefault();
     return false;
   };
 
   const drop = (e, instance) => {
-    const { docViewer } = instance;
-    const scrollElement = docViewer.getScrollViewElement();
+    const { docViewer = false } = instance;
+    const scrollElement = docViewer?.getScrollViewElement();
     const scrollLeft = scrollElement.scrollLeft || 0;
     const scrollTop = scrollElement.scrollTop || 0;
     setDropPoint({ x: e.pageX + scrollLeft, y: e.pageY + scrollTop });
@@ -100,24 +95,28 @@ const PlaceField = ({
     return false;
   };
 
-  const dragStart = e => {
+  const dragStart = (e) => {
     e.target.style.opacity = 0.5;
     const copy = e.target.cloneNode(true);
-    copy.id = 'form-build-drag-image-copy';
-    copy.style.width = '250px';
+    copy.id = "form-build-drag-image-copy";
+    copy.style.width = "250px";
     document.body.appendChild(copy);
     e.dataTransfer.setDragImage(copy, 125, 25);
-    e.dataTransfer.setData('text', '');
+    e.dataTransfer.setData("text", "");
   };
 
   const dragEnd = (e, type) => {
     addField(type, dropPoint);
     e.target.style.opacity = 1;
     document.body.removeChild(
-      document.getElementById('form-build-drag-image-copy'),
+      document.getElementById("form-build-drag-image-copy")
     );
     e.preventDefault();
   };
+
+  useEffect(() => {
+    console.log("assi", assignee);
+  }, [assignee]);
 
   const addField = (type, autoFill = false, point = {}, value) => {
     const { docViewer, Annotations } = instance;
@@ -125,9 +124,10 @@ const PlaceField = ({
     const doc = docViewer.getDocument();
     const displayMode = docViewer.getDisplayModeManager().getDisplayMode();
     const page = displayMode.getSelectedPages(point, point);
-    if (!!point.x && page.first == null) return  //don't add field to an invalid page location
+    if (!!point?.x && page.first == null) return; //don't add field to an invalid page location
 
-    const page_idx = page.first !== null ? page.first : docViewer.getCurrentPage();
+    const page_idx =
+      page.first !== null ? page.first : docViewer.getCurrentPage();
     const page_info = doc.getPageInfo(page_idx);
     const page_point = displayMode.windowToPage(point, page_idx);
     const zoom = docViewer.getZoom();
@@ -148,7 +148,8 @@ const PlaceField = ({
 
     textAnnot.setPadding(new Annotations.Rect(0, 0, 0, 0));
     // TODO default field placeholder
-    var defaultValue = value ?? `${assignee}_${type}_`; 
+
+    var defaultValue = value ?? `${assignee.value} ${type}`;
     textAnnot.custom = {
       id: null,
       type,
@@ -158,12 +159,12 @@ const PlaceField = ({
 
     // set the type of annot
     textAnnot.setContents(textAnnot.custom.value);
-    textAnnot.FontSize = '' + 20.0 / zoom + 'px';
+    textAnnot.FontSize = "" + 16.0 / zoom + "px";
     textAnnot.FillColor = new Annotations.Color(211, 211, 211, 0.5);
     textAnnot.TextColor = new Annotations.Color(0, 165, 228);
     textAnnot.StrokeThickness = 1;
     textAnnot.StrokeColor = new Annotations.Color(0, 165, 228);
-    textAnnot.TextAlign = 'center';
+    textAnnot.TextAlign = "center";
 
     textAnnot.Author = annotManager.getCurrentUser();
 
@@ -173,7 +174,8 @@ const PlaceField = ({
     annotManager.selectAnnotation(textAnnot);
   };
 
-  const applyFields = async () => { // TODO pindahin ke ReviewSend.js
+  const applyFields = async () => {
+    // TODO pindahin ke ReviewSend.js
     const { Annotations, docViewer } = instance;
     const annotManager = docViewer.getAnnotationManager();
     const fieldManager = annotManager.getFieldManager();
@@ -185,23 +187,22 @@ const PlaceField = ({
       annotationsList.map(async (annot, index) => {
         let inputAnnot;
         let field;
-        if (typeof annot.custom === 'undefined') return
+        if (typeof annot.custom === "undefined") return;
 
         // create a form field based on the type of annotation
-        if (annot.custom.type === 'SIGNATURE') {
+        if (annot.custom.type === "SIGNATURE") {
           field = new Annotations.Forms.Field(
             annot.getContents() + Date.now() + index,
             {
-              type: 'Sig',
-            },
+              type: "Sig",
+            }
           );
           inputAnnot = new Annotations.SignatureWidgetAnnotation(field, {
-            appearance: '_DEFAULT',
+            appearance: "_DEFAULT",
             appearances: {
               _DEFAULT: {
                 Normal: {
-                  data:
-                    'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjEuMWMqnEsAAAANSURBVBhXY/j//z8DAAj8Av6IXwbgAAAAAElFTkSuQmCC',
+                  data: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjEuMWMqnEsAAAANSURBVBhXY/j//z8DAAj8Av6IXwbgAAAAAElFTkSuQmCC",
                   offset: {
                     x: 100,
                     y: 100,
@@ -210,17 +211,17 @@ const PlaceField = ({
               },
             },
           });
-        } else if (annot.custom.type === 'DATE') {
+        } else if (annot.custom.type === "DATE") {
           field = new Annotations.Forms.Field(
             annot.getContents() + Date.now() + index,
             {
-              type: 'Tx',
-              value: 'd-m-yyyy',
+              type: "Tx",
+              value: "d-m-yyyy",
               // Actions need to be added for DatePickerWidgetAnnotation to recognize this field.
               actions: {
                 F: [
                   {
-                    name: 'JavaScript',
+                    name: "JavaScript",
                     // You can customize the date format here between the two double-quotation marks
                     // or leave this blank to use the default format
                     javascript: 'AFDate_FormatEx("dd-mm-yyyy");', // TODO change date format if needed
@@ -228,33 +229,33 @@ const PlaceField = ({
                 ],
                 K: [
                   {
-                    name: 'JavaScript',
+                    name: "JavaScript",
                     // You can customize the date format here between the two double-quotation marks
                     // or leave this blank to use the default format
                     javascript: 'AFDate_FormatEx("dd-mm-yyyy");', // TODO change date format if needed
                   },
                 ],
               },
-            },
+            }
           );
 
           inputAnnot = new Annotations.DatePickerWidgetAnnotation(field);
-        } else if (annot.custom.type === 'CHECKBOX') {
+        } else if (annot.custom.type === "CHECKBOX") {
           field = new Annotations.Forms.Field(
             annot.getContents() + Date.now() + index,
             {
-              type: 'Tx',
+              type: "Tx",
               value: annot.custom.value,
-            },
+            }
           );
           inputAnnot = new Annotations.CheckButtonWidgetAnnotation(field);
         } else {
           field = new Annotations.Forms.Field(
             annot.getContents() + Date.now() + index,
             {
-              type: 'Tx',
+              type: "Tx",
               value: annot.custom.value,
-            },
+            }
           );
           inputAnnot = new Annotations.TextWidgetAnnotation(field);
         }
@@ -281,7 +282,7 @@ const PlaceField = ({
         Annotations.WidgetAnnotation.getCustomStyles = function (widget) {
           if (widget instanceof Annotations.SignatureWidgetAnnotation) {
             return {
-              border: '1px solid #a5c7ff',
+              border: "1px solid #a5c7ff",
             };
           }
         };
@@ -291,7 +292,7 @@ const PlaceField = ({
         annotManager.addAnnotation(inputAnnot);
         fieldManager.addField(field);
         annotsToDraw.push(inputAnnot);
-      }),
+      })
     );
 
     // delete old annotations
@@ -304,167 +305,60 @@ const PlaceField = ({
   };
   // END JOJO
 
+  const ConferArea = ({ data }) => (
+    <div className="px-2 py-1">
+      <div
+        draggable
+        onDragStart={(e) => dragStart(e)}
+        onDragEnd={(e) => dragEnd(e, data[0])}
+      >
+        <button
+          className="button-placefields"
+          onClick={() => addField(data[0], false)}
+        >
+          {data[0]}
+        </button>
+      </div>
+    </div>
+  );
+
   return (
-    <div className={"prepareDocument"}>
+    <div className={"place-field-area"}>
       <div className="row">
-        {/* <div className="col-2">
-          <h1>View Document</h1>          
-        </div> */}
-        {/* JOJO */}
+        <div className="col-lg-3 col-md-12 left-sidebar">
+          <div className="container">
+            <div className="row pt-2">
+              <div className="lead">Signers</div>
+              <select value={assignee} onChange={(val) => setAssignee(val)}>
+                {assigneesValues &&
+                  assigneesValues?.map((assignee, i) => (
+                    <option key={i} value={assignee} data-before={"red"}>
+                      {assignee.label}
+                    </option>
+                  ))}
+              </select>
 
-        <div className="col-3">
-          <div className="row">
-            <div className="lead">Signers</div>
-            <Box padding={2}>
-              <SelectList
-                id="assigningFor"
-                name="assign"
-                onChange={({ value }) => setAssignee(value)}
-                options={assigneesValues}
-                placeholder="Select recipient"
-                value={assignee}
-              />
-            </Box>
+              <div className="lead">Signature fields</div>
+              {[["SIGNATURE"], ["INITIAL"]]?.map((data, i) => (
+                <ConferArea data={data} key={i} />
+              ))}
 
-            <div className="lead">Signature fields</div>
-            <Box padding={2}>
-              <div
-                draggable
-                onDragStart={e => dragStart(e)}
-                onDragEnd={e => dragEnd(e, 'SIGNATURE')}
-              >
-                {/* TODO add signature icon */}
-                <Button
-                  onClick={() => addField('SIGNATURE', false)}
-                  accessibilityLabel="add signature"
-                  text="Signature"
-                />
-              </div>
-            </Box>
-            <Box padding={2}>
-              <div
-                draggable
-                onDragStart={e => dragStart(e)}
-                onDragEnd={e => dragEnd(e, 'INITIAL')}
-              >
-                {/* TODO add initials icon */}
-                <Button
-                  onClick={() => addField('INITIAL', false)}
-                  accessibilityLabel="add initial"
-                  text="Initials"
-                />
-              </div>
-            </Box>
+              <div className="lead">Auto-fill fields</div>
+              {[["DATE"], ["NAME"], ["EMAIL"], ["COMPANY"], ["TITLE"]]?.map(
+                (data, i) => (
+                  <ConferArea key={i} data={data} />
+                )
+              )}
 
-            <div className="lead">Auto-fill fields</div>
-            <Box padding={2}>
-              <div
-                draggable
-                onDragStart={e => dragStart(e)}
-                onDragEnd={e => dragEnd(e, 'DATE')}
-              >
-                {/* TODO add dates icon */}
-                <Button
-                  onClick={() => addField('DATE', true)}
-                  accessibilityLabel="add date"
-                  text="Dates"
-                />
-              </div>
-            </Box>
-            <Box padding={2}>
-              <div
-                draggable
-                onDragStart={e => dragStart(e)}
-                onDragEnd={e => dragEnd(e, 'NAME')}
-              >
-                {/* TODO add name icon */}
-                <Button
-                  onClick={() => addField('NAME', true)}
-                  accessibilityLabel="add name"
-                  text="Name"
-                />
-              </div>
-            </Box>
-            <Box padding={2}>
-              <div
-                draggable
-                onDragStart={e => dragStart(e)}
-                onDragEnd={e => dragEnd(e, 'EMAIL')}
-              >
-                {/* TODO add email icon */}
-                <Button
-                  onClick={() => addField('EMAIL', true)}
-                  accessibilityLabel="add email"
-                  text="Email"
-                />
-              </div>
-            </Box>
-            <Box padding={2}>
-              <div
-                draggable
-                onDragStart={e => dragStart(e)}
-                onDragEnd={e => dragEnd(e, 'COMPANY')}
-              >
-                {/* TODO add company icon */}
-                <Button
-                  onClick={() => addField('COMPANY', true)}
-                  accessibilityLabel="add company"
-                  text="Company"
-                />
-              </div>
-            </Box>
-            <Box padding={2}>
-              <div
-                draggable
-                onDragStart={e => dragStart(e)}
-                onDragEnd={e => dragEnd(e, 'TITLE')}
-              >
-                {/* TODO add title icon */}
-                <Button
-                  onClick={() => addField('TITLE', true)}
-                  accessibilityLabel="add title"
-                  text="Title"
-                />
-              </div>
-            </Box>
-
-            <div className="lead">Standard fields</div>
-            {/* TODO add lock icon, set disabled */}
-            <Box padding={2}>
-              <div
-                draggable
-                onDragStart={e => dragStart(e)}
-                onDragEnd={e => dragEnd(e, 'TEXT')}
-              >
-                {/* TODO add textbox icon */}
-                <Button
-                  onClick={() => addField('TEXT', true)}
-                  accessibilityLabel="add text"
-                  text="Textbox"
-                />
-                {/* TODO add lock icon, set disabled */}
-              </div>
-            </Box>
-            <Box padding={2}>
-              <div
-                draggable
-                onDragStart={e => dragStart(e)}
-                onDragEnd={e => dragEnd(e, 'CHECKBOX')}
-              >
-                {/* TODO add checkbox icon */}
-                <Button
-                  onClick={() => addField('CHECKBOX', true)}
-                  accessibilityLabel="add checkbox"
-                  text="Checkbox"
-                />
-                {/* TODO add lock icon, set disabled */}
-              </div>
-            </Box>
+              <div className="lead">Standard fields</div>
+              {[["TEXT"], ["CHECKBOX"]]?.map((data, i) => (
+                <ConferArea key={i} data={data} />
+              ))}
+            </div>
           </div>
         </div>
-        {/* END JOJO */}
 
-        <div className="col-9">
+        <div className="col-lg-9 col-md-12">
           <div className="webviewer" ref={viewer}></div>
         </div>
       </div>
